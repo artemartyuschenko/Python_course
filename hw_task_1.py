49b40ad62f239a2507a773977fadc45c60ce6638
artemartyuschenko <151003582+artemartyuschenko@users.noreply.github.com>
2023-11-16 11:44:10 +0300

    Update hw_task_1.py

--- hw_task_1.py
+++ hw_task_1.py
@@ -1,9 +1,11 @@
-def length(my_str): #Task 1
+#Task 1
+def length(my_str):
     return len(my_str)
 
 print(length("London is the capital of Great Britain"))
 
-def lists_length_compare(my_list1, my_list2): #Task 2
+#Task 2
+def lists_length_compare(my_list1, my_list2):
     if len(my_list1) > len(my_list2):
         return len(my_list1)
     else:
@@ -11,13 +13,15 @@ def lists_length_compare(my_list1, my_list2): #Task 2
     
 print(lists_length_compare([1,2,3],[1,2,3,4,5,6,7,8]))
 
-def add_to_list(my_list,*args): #task  3
+#Task  3
+def add_to_list(my_list,*args):
     my_list.append(*args)
     return my_list
 
 print(add_to_list([1,"dfdfdf",'t',1.0,1000000],"my_value"))
 
-def more_or_less_100(my_value): #task  4
+#Task  4
+def more_or_less_100(my_value):
     if my_value > 100 or my_value < -100:
         print("-")
     else:
@@ -26,7 +30,8 @@ def more_or_less_100(my_value): #task  4
 more_or_less_100(200)
 more_or_less_100(99)
 
-def yes_or_not(my_str_1, my_str_2): #task  5
+#Task  5
+def yes_or_not(my_str_1, my_str_2):
     for i in range(len(my_str_1)):
         if my_str_1[i] != my_str_2[i]:
             break
